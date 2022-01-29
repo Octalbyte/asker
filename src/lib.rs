7fc37c2804c8311d4b3a9891e4cb9cae4bddaf63
jp <jpso.guerreiro@gmail.com>
2022-01-29 12:23:35 -0300

    Its working

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub struct Asker{
-	fields: Vec<(String, bool, bool, String, dyn Fn(&str) -> bool)>	
+	fields: Vec<(String, bool, bool, String, Box<dyn Fn(&str) -> bool>)>	
 }
 
-fn truish(t: &str){
+fn truish(t: &str) -> bool{
 	return true;
 }
 
@@ -13,9 +13,23 @@ impl Asker {
 		};	
 	}
 
-	pub fn add(&mut self, ) {
-		self.fields.push();
+	pub fn add(&mut self, name: &str, opts: Vec<&str>, check: Option<Box<dyn Fn(&str) -> bool>>) {
+	let mut hidden = false;
+	if opts.iter().any(|&i| i=="hidden") {
+    	hidden = true;
 	}
+	let mut confirm = false;
+	if opts.iter().any(|&i| i=="hidden") {
+    	confirm = true;
+	}
+	let mut helpcheck = "";
+	for i in opts.iter() {
+		if i != &"hidden" && i != &"confirm"{
+			helpcheck = i 
+		}
+	}
+		
+}
 
 	pub fn boot(){
 
