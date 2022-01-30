2c6c2693a8ba3cbc3b46857a2a6bb122f8fb6534
Octalbyte <octalbyte0o52@protonmail.com>
2022-01-30 17:02:36 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,32 @@ use regex::Regex;
 	) {
 
 		for i in fields.iter(){
-			let name, opts, checker = i;
+			let (name, opts, checker) = i;
+			let mut hidden = false;
+			let mut confirm = false;
+			let mut req = "";
+			
+			for wrd in opts.iter(){
+				match wrd {
+					"hidden" => {
+						hidden = true;	
+					},
+					"confirm" => {
+						confirm = true;	
+					},
+					a => {
+						req = a;
+					}
+						
+				}
+			}
+			
+			
+			let reg = match checker {
+				Some(exp) => exp,
+				None => Regex::new(r".*")
+			}
+			
 		}
 
 	}
