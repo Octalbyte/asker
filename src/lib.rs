8b185c73f331b1e082ae4f5beee2c3418e69c1ef
jp <jpso.guerreiro@gmail.com>
2022-01-31 12:05:50 -0300

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,79 @@
 use regex::Regex;
 use std::io;
 use std::io::Write;
+use std::collections::HashMap;
 
 	pub fn ask(
 		fields: Vec<(&str, Vec<&str>, Option<Regex>)>	
-	) {
+	) -> (HashMap<String, String>, HashMap<String, bool>){
+
+		let mut str_matches: HashMap<String, String> = HashMap::new();
+		let mut bool_matches: HashMap<String, bool> = HashMap::new();
 
 		for i in fields.iter(){
 			let (name, opts, checker) = i;
 			let mut hidden = false;
 			let mut confirm = false;
-			let mut req = "";
-			
+			let mut isbool = false;
+			let mut default = None; 
+			let mut req = None;
+			let mut id = name.to_owned();
+			let mut stuff: Vec<&str> = vec![];
+
 			for wrd in opts.iter(){
 				match wrd {
-					"hidden" => {
+					&"hidden" => {
 						hidden = true;	
 					},
-					"confirm" => {
+					&"confirm" => {
 						confirm = true;	
 					},
-					a => {
-						req = a;
+					&"isbool" => {
+						isbool = true;
 					}
-						
+					a => { 
+						let b: String = String::from(a.to_owned());
+						if b.starts_with("default:"){
+							let s = b.replacen("default:","",1);
+							let s = s.as_str();
+							stuff.push(format!("{}", s.clone()).as_str());
+							default = Some(stuff[0]);
+						}
+						if b.starts_with("req:"){
+							req = Some(b.replacen("req:", "",1).as_str());
+						}
+						if b.starts_with("id:"){
+							id = b.replacen("id:", "",1).as_str();
+						}
+					}		
 				}
 			}
 			
-			
+			let match_any = Regex::new(r".*").unwrap();
 			let reg = match checker {
 				Some(exp) => exp,
-				None => Regex::new(r".*")
+				None => {
+					&match_any
+				}
+			};
+			
+			if isbool {
+				safe_print(name);
+				match default {
+					Some("true") => {
+						safe_print(" (Y/n): ");
+					},
+					Some("false") => {
+						safe_print(" (y/N): ");
+					}
+					Some(_) => {
+						panic!("Invalid default for bool: Accepted values: false, true");
+					}
+					None => {
+						safe_print(" (y/n): ");
+					}
+				}
+
 			}
 			/*
 			print!("Enter a number: ");
@@ -43,5 +86,10 @@ use std::io::Write;
 			*/
 			
 		}
+		return (str_matches, bool_matches);
+	}
 
+	fn safe_print(a1: &str){
+		print!("{}", a1);
+		io::stdout().flush().unwrap();
 	}
